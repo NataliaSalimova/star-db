1c050dea9e478b2a75cf0f4a32a10a4b6a864673
nsalimova <nsalimova@oggettoweb.com>
2023-10-16 12:27:21 +0300

    Higher order components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,11 +4,11 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import ErrorBoundry from '../error-boundry';
 import ErrorIndicator from '../error-indicator';
+import ItemDetails, { Record }  from '../item-details/item-details';
+import ItemList from '../item-list';
 
 import './app.css';
 import SwapiService from '../../services/swapi-service';
-import ItemDetails, { Record }  from '../item-details/item-details';
-import Row from '../row';
 
 export default class App extends Component {
     swapiService = new SwapiService();
@@ -29,7 +29,7 @@ export default class App extends Component {
 
         const planet = this.state.showRandomPlanet ? <RandomPlanet/> : null;
 
-        const { getPerson, getStarship, getPersonImage, getStarshipsImage } = this.swapiService;
+        const { getPerson, getStarship, getPersonImage, getStarshipsImage, getAllPeople, getAllPlanets } = this.swapiService;
         const personDetails = (
             <ItemDetails
                 itemId={11}
@@ -54,10 +54,17 @@ export default class App extends Component {
             <ErrorBoundry>
                 <div className="stardb-app">
                     <Header/>
-                    <Row
-                        left={personDetails}
-                        right={starshipDetails}
-                    />
+                    <ItemList
+                        getData={getAllPeople}
+                        onItemSelected={()=> {}}>
+                        {({name}) => <span>{name}</span>}
+                    </ItemList>
+
+                    <ItemList
+                        getData={getAllPlanets}
+                        onItemSelected={()=> {}}>
+                        {({name}) => <span>{name}</span>}
+                    </ItemList>
                 </div>
 
             </ErrorBoundry>
--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,55 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-import Spinner from '../spinner';
+import { withData } from '../hoc-helpers';
 
 import './item-list.css';
 
-export default class ItemList extends Component {
+import SwapiService from '../../services/swapi-service';
 
-    state = {
-        itemList: null
-    }
+const ItemList = (props) => {
+    const { data, onItemsSelected, children: renderLabel } = props;
 
-    componentDidMount() {
-        const { getData } = this.props;
-
-        getData()
-            .then((itemList)=> {
-                this.setState({
-                    itemList
-                });
-            });
-    }
-
-    renderItems = (arr) => {
-        return arr.map((item) => {
-            const { id } = item;
-            const label = this.props.children(item);
-
-            return (
-                <li
-                    className="list-group-item"
-                    key={id}
-                    onClick={()=>this.props.onItemsSelected(id)}>
-                    {label}
-                </li>
-            )
-        })
-    }
-
-    render() {
-        const { itemList } = this.state;
-
-        if (!itemList) {
-            return <Spinner/>;
-        }
-
-        const items = this.renderItems(itemList);
+    const items = data.map((item) => {
+        const { id } = item;
+        const label = renderLabel(item);
 
         return (
-            <ul className="item-list list-group">
-                {items}
-            </ul>
-        );
-    }
+            <li
+                className="list-group-item"
+                key={id}
+                onClick={()=>onItemsSelected(id)}>
+                {label}
+            </li>
+        )
+    })
+
+    return (
+        <ul className="item-list list-group">
+            {items}
+        </ul>
+    );
 }
+
+const { getAllPeople} = new SwapiService();
+
+export default withData(ItemList, getAllPeople);
